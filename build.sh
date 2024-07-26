37ca87d110ad0b422097a5658a1d7b8bd95f396d
gsilva2016 <gabriel.l.silva@intel.com>
2024-07-26 14:38:21 -0700

    ipex build option

--- build.sh
+++ build.sh
@@ -4,9 +4,7 @@ echo "Building mlc-ai container"
 docker build -t mlc-ai:1.0 .
 
 echo "Building ipex-llm container"
-docker build -t ipex-llm:1.0 -f Dockerfile.ipexllm .
+docker build -t ipex-llm:1.0 -f Dockerfile.ipexllm . 
 
-
-#echo "Building itrex container"
-#docker build -t itrex:1.0 -f Dockerfile.itrex .
-
+echo "Building ipex-xpu container"
+docker build -t ipex-xpu:1.0 -f Dockerfile.ipex-xpu .
